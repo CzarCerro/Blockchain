2b02278994a5e8bfc3f7ca9b786deabf8c556407
CzarCerro <abramobligacion@gmail.com>
2022-05-06 02:57:05 +0800

    Add blockchain algorithm class

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
+const SHA256 = require('crypto-js/sha256')
+
 class Block {
     constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previoushash = previousHash;
-        this.hash = '';
+        this.hash = this.calculateHash;
+    }
+
+    calculateHash(){
+        return SHA256(this.index+this.previoushash+this.timestamp+JSON.stringify(this.data)).toString();
     }
 }
+
+class Blockchain{
+    constructor(){
+        this.chain = [];
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "06/05/2022", "Genesis block", "0")
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
