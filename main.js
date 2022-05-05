48fbfea82ebce6a48de95eb61e609bd04dc1b0b5
CzarCerro <abramobligacion@gmail.com>
2022-05-06 03:51:09 +0800

    validate chain

--- main.js
+++ main.js
@@ -7,5 +7,6 @@ ExampleCoin.addBlock(new Block(1, "12/12/2022", {amount: 4}))
 ExampleCoin.addBlock(new Block(2, "13/12/2022", {amount: 10}))
 ExampleCoin.addBlock(new Block(2, "14/12/2022", {amount: 15}))
 
-console.log(JSON.stringify(ExampleCoin, null, 4));
+//console.log(JSON.stringify(ExampleCoin,null,4))
+console.log(ExampleCoin.isChainValid());
 
